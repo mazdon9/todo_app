fe066f2110ecbff35b6f6c246fe9fb41f5cbf07e
mazdon9 <chemanhkiett@gmail.com>
2025-07-18 19:17:22 +0700

    message: Changes Task02: Registration Screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: todoApp(),
+      home: TodoApp(),
     );
   }
 }
 
-class todoApp extends StatelessWidget {
-  const todoApp({super.key});
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +54,7 @@ class todoApp extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.only(left: 48.0, right: 46.0),
             child: Text(
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum dictum tempus, interdum at dignissim metus. Ultricies sed nunc.',
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elsit. Interdum dictum tempus, interdum at dignissim metus. Ultricies sed nunc.',
               textAlign: TextAlign.center, // Canh trái văn bản
               style: TextStyle(
                 fontFamily: 'Poppins',
@@ -66,7 +66,10 @@ class todoApp extends StatelessWidget {
           SizedBox(height: 92),
           GestureDetector(
             onTap: () {
-              print('Tapping');
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => RegistrationScreen()),
+              );
             },
             child: Container(
               width: 325,
@@ -85,11 +88,225 @@ class todoApp extends StatelessWidget {
               ),
             ),
           ),
-          //   Padding(
-          //     padding: const EdgeInsets.only(left: 48.0, right: 46.0),
-          //   ),
         ],
       ),
     );
+  }
+}
+
+class RegistrationScreen extends StatelessWidget {
+  const RegistrationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF6F6F6),
+      body: Column(
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Image.asset('assets/images/shape.png'),
+          ),
+          SizedBox(height: 76),
+          Container(
+            child: Text(
+              'Welcome Onboard!',
+              style: TextStyle(
+                fontFamily: 'Poppins-Regular',
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 2,
+              ),
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            child: Text(
+              'Let’s help you meet up your tasks!',
+              style: TextStyle(
+                fontFamily: 'Poppins-Regular',
+                fontSize: 13,
+                fontWeight: FontWeight.w400,
+                letterSpacing: 2,
+                color: Color(0xff565656),
+              ),
+            ),
+          ),
+          SizedBox(height: 49),
+          Align(
+            alignment: Alignment.center,
+            child: SizedBox(
+              width: 325,
+              height: 521,
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xffFFFFFF),
+                      hintText: 'Enter your full name',
+                      hintStyle: TextStyle(
+                        fontFamily: 'Poppins-Regular',
+                        fontSize: 13,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 2,
+                        color: Color(0xff565656),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 30,
+                        vertical: 16,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 21),
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xffFFFFFF),
+                      hintText: 'Enter your email',
+                      hintStyle: TextStyle(
+                        fontFamily: 'Poppins-Regular',
+                        fontSize: 13,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 2,
+                        color: Color(0xff565656),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 30,
+                        vertical: 16,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 21),
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xffFFFFFF),
+                      hintText: 'Enter your password',
+                      hintStyle: TextStyle(
+                        fontFamily: 'Poppins-Regular',
+                        fontSize: 13,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 2,
+                        color: Color(0xff565656),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 30,
+                        vertical: 16,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 21),
+                  TextField(
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Color(0xffFFFFFF),
+                      hintText: 'Confirm password',
+                      hintStyle: TextStyle(
+                        fontFamily: 'Poppins-Regular',
+                        fontSize: 13,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 2,
+                        color: Color(0xff565656),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide.none,
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 30,
+                        vertical: 16,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()),
+                      );
+                    },
+                    child: Container(
+                      width: 325,
+                      height: 62,
+                      decoration: BoxDecoration(color: Color(0xff50C2C9)),
+                      child: Center(
+                        child: Text(
+                          'Register',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            letterSpacing: 2,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 23),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Already have an account ?',
+                        style: TextStyle(
+                          fontFamily: 'Poppins-Regular',
+                          fontSize: 13,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 2,
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => LoginScreen(),
+                            ),
+                          );
+                        },
+
+                        child: Text(
+                          ' Sign in',
+                          style: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontSize: 13,
+                            fontWeight: FontWeight.w600,
+                            letterSpacing: 2,
+                            color: Color(0xff50C2C9),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
